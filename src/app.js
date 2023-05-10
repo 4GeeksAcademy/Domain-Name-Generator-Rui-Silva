c438f91a9e4922e8474a3403559f6ca45d61fd50
Rui Silva <rsilva6420@gmail.com>
2023-05-10 11:11:45 +0000

    final version - with html deployment

--- src/app.js
+++ src/app.js
@@ -1,25 +1,25 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let pronoun = ["the", "our", "your"];
+let adj = ["great", "big", "worst"];
+let noun = ["jogger", "racoon", "game"];
+let extensions = [".com", ".pt", ".us"];
+let aux = [];
 
-window.onload = function() {
-  document.getElementById("domainGenerator").innerHTML = domainNameGenerator();
-};
-
-let domainNameGenerator = () => {
-  let pronoun = ["the", "our"];
-  let adj = ["great", "big"];
-  let noun = ["jogger", "racoon"];
-  let extensions = [".com", ".pt", ".us"];
-  for (let i = 0; i < pronoun.length; i++) {
-    for (let j = 0; j < adj.length; j++) {
-      for (let k = 0; k < noun.length; k++) {
-        for (let l = 0; l < extensions.length; l++) {
-          console.log(pronoun[i] + adj[j] + noun[k] + extensions[l]);
-        }
+for (let i = 0; i < pronoun.length; i++) {
+  for (let j = 0; j < adj.length; j++) {
+    for (let k = 0; k < noun.length; k++) {
+      for (let l = 0; l < extensions.length; l++) {
+        aux.push(pronoun[i] + adj[j] + noun[k] + extensions[l]);
       }
     }
   }
-};
+}
+
+let list = document.getElementById("domainGenerator");
+for (let m = 0; m < aux.length; ++m) {
+  let li = document.createElement("li");
+  li.innerText = aux[m];
+  list.appendChild(li);
+}
